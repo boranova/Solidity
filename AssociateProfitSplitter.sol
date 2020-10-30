f40693e5a2df769d646ca818071f79c2f96afbe2
boranova <65459829+boranova@users.noreply.github.com>
2020-10-30 14:48:16 -0500

    Update AssociateProfitSplitter.sol

--- AssociateProfitSplitter.sol
+++ AssociateProfitSplitter.sol
@@ -1,8 +1,18 @@
 pragma solidity ^0.5.0;
 
+import "github.com/OpenZeppelin/openzeppelin-contracts/blob/release-v2.5.0/contracts/math/SafeMath.sol";
+
 // lvl 1: equal split
 contract AssociateProfitSplitter {
+     using SafeMath for uint;
+     
     // @TODO: Create three payable addresses representing `employee_one`, `employee_two` and `employee_three`.
+    
+    address payable employee_one;
+    address payable employee_two;
+    address payable employee_three;
+
+
 
     constructor(address payable _one, address payable _two, address payable _three) public {
         employee_one = _one;
@@ -16,13 +26,16 @@ contract AssociateProfitSplitter {
 
     function deposit() public payable {
         // @TODO: Split `msg.value` into three
-        uint amount = ; // Your code here!
+        uint amount = msg.value.div(3); 
 
         // @TODO: Transfer the amount to each employee
-        // Your code here!
+        employee_one.transfer(amount);
+        employee_two.transfer(amount);
+        employee_three.transfer(amount);
 
         // @TODO: take care of a potential remainder by sending back to HR (`msg.sender`)
-        // Your code here!
+        
+        msg.sender.transfer(msg.value - amount.mul(3));
     }
 
     function() external payable {
