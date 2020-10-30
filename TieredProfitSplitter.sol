79db905072d803f25f0a447c1908062e74edb41b
boranova <65459829+boranova@users.noreply.github.com>
2020-10-30 15:02:02 -0500

    Update TieredProfitSplitter.sol

--- TieredProfitSplitter.sol
+++ TieredProfitSplitter.sol
@@ -1,7 +1,11 @@
 pragma solidity ^0.5.0;
 
+import "github.com/OpenZeppelin/openzeppelin-contracts/blob/release-v2.5.0/contracts/math/SafeMath.sol";
+
 // lvl 2: tiered split
 contract TieredProfitSplitter {
+    using SafeMath for uint;
+       
     address payable employee_one; // ceo
     address payable employee_two; // cto
     address payable employee_three; // bob
@@ -24,11 +28,23 @@ contract TieredProfitSplitter {
 
         // @TODO: Calculate and transfer the distribution percentage
         // Step 1: Set amount to equal `points` * the number of percentage points for this employee
+        amount = points.mul(0.6);
         // Step 2: Add the `amount` to `total` to keep a running total
+        total +=amount;
         // Step 3: Transfer the `amount` to the employee
-
+        employee_one.transfer(amount);
+        
         // @TODO: Repeat the previous steps for `employee_two` and `employee_three`
         // Your code here!
+        
+        amount = points.mul(0.15);
+        total +=amount;
+        employee_two.transfer(amount);
+        
+        amount = points.mul(0.25);
+        total +=amount;
+        employee_three.transfer(amount);
+    
 
         employee_one.transfer(msg.value - total); // ceo gets the remaining wei
     }
@@ -36,4 +52,3 @@ contract TieredProfitSplitter {
     function() external payable {
         deposit();
     }
-}
